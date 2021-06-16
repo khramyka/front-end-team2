ff75a129ef7f4123feab34c94079d8a45e917e6c
Volodymyr Sheremeta <volosher@gmail.com>
2021-06-17 00:04:02 +0300

    Layout and ExtendedCard created

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import ResponsiveDrawer from "../ResponsiveDrawer/ResponsiveDrawer";
+import './Layout.scss'
 import Header from '../../../components/Header/Header';
 import Footer from "../../Footer/Footer";
 import SearchBar from "../SearchBar/SearchBar";
+import ExtendedCard from "../../card/ExtendedCard/ExtendedCard";
+import Grid from "@material-ui/core/Grid";
+import SaleCard from "../../card/SaleCard/SaleCard";
+import FilterListIcon from '@material-ui/icons/FilterList';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -11,7 +15,6 @@ const useStyles = makeStyles((theme) => ({
         maxWidth: 1200,
         margin: '0 auto',
         flexGrow: 1,
-
     },
 
     /* breakpoints: {
@@ -31,18 +34,65 @@ const useStyles = makeStyles((theme) => ({
 export default function FullWidthGrid() {
     const classes = useStyles();
 
+    const handleClick = () => {
+
+        const myElement: HTMLElement | null = document.querySelector('.ExtendedCard');
+        if (myElement === null) {
+            console.log('Fuck')
+        } else {
+                myElement.style.display = 'block';
+            }
+
+    };
+
+    const filterClick = () => {
+        const myElement: HTMLElement | null = document.querySelector('.filterBar');
+        if (myElement === null) {
+            console.log('Fuck')
+        } else {
+            myElement.classList.toggle("open");
+        }
+    };
+
     return (
         <div   className={classes.root}>
             <Header />
 
-            <div id="drawer-container" style={{position: 'relative'}}>
 
+            <div className="mainContent" style={{position: 'relative'}}>
+                <div className="filterButton" onClick={filterClick}>
+                    <FilterListIcon
+                        fontSize="large"/>
+                </div>
 
-                <ResponsiveDrawer    />
+              <div className="filterBar">
+                <SearchBar/>
+              </div>
+              <div className="cards">
+                  <ExtendedCard />
+                  <Grid
 
+                      container
+                      spacing={3}
+                      justify="center"
+                  >
+                      {/* <ExtendedCard/>*/}
+                      {[0, 1, 2, 3, 4, 5].map((value) => (
+                          <Grid key={value} item
+                          onClick={handleClick}>
+                              <SaleCard
+
+                              />
+                          </Grid>
+                      ))}
+                  </Grid>
+              </div>
 
 
             </div>
+
+
+
             <Footer/>
 
         </div>
